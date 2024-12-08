ef053ca7f00bf754cd8e2fe4a43e69d3c2ef3071
Luke McDowell <luke.db.mcdowell@gmail.com>
2024-12-08 17:32:32 +0000

    fixing skeleton sizes. adding copy to home page

--- src/components/TopTrackSkeleton.tsx
+++ src/components/TopTrackSkeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from './ui/skeleton';
 
 function TopTrackSkeleton() {
   return (
-    <Card className="flex items-center gap-3 p-2 w-full max-w-sm border hover:bg-zinc-900 hover:border-primary cursor-pointer">
+    <Card className="flex items-center gap-3 p-2 w-[286px] max-w-sm border hover:bg-zinc-900 hover:border-primary cursor-pointer">
       <Skeleton id="img-skeleton" className="h-16 w-16 rounded" />
       <div className="min-w-0 flex-1">
         <Skeleton
